76e1299668579976817d3dc6f0b1c327a2627dd0
paractis <danir23.007@gmail.com>
2025-10-29 01:08:16 +0000

    Commit de haber terminado emparejado backend con front

--- cronox-backend/src/app.module.ts
+++ cronox-backend/src/app.module.ts
@@ -3,8 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..'), // raíz del repo
+      exclude: ['/api*', '/products*'],
+      serveStaticOptions: { index: 'index.html' },
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
