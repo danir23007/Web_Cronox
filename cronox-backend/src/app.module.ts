4a22ee7732bfd67a52f983ef12652a76c2a73750
paractis <danir23.007@gmail.com>
2025-10-31 00:11:13 +0100

    Integrate Prisma with products module

--- cronox-backend/src/app.module.ts
+++ cronox-backend/src/app.module.ts
@@ -2,18 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', '..'), // raíz del repo
-      exclude: ['/api*', '/products*'],
+      rootPath: join(__dirname, '..', '..'),
+      exclude: ['/api(.*)', '/products(.*)', '/products', '/api'],
       serveStaticOptions: { index: 'index.html' },
     }),
     ProductsModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
